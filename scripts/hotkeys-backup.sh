414ce06a630495f2d9f706c15b0e40ac2cf185b2
Philippe Lafrance <phil.laf@gmail.com>
2025-05-28 00:16:43 -0400

    Add gnome interface configs + fix workspace hotkeys

--- scripts/hotkeys-backup.sh
+++ scripts/hotkeys-backup.sh
@@ -6,10 +6,13 @@ mkdir -p "$BACKUP_DIR"
 echo "- Dumping ALL media keys (including default + custom)..."
 dconf dump /org/gnome/settings-daemon/plugins/media-keys/ > "$BACKUP_DIR/media-keys.dconf"
 
-echo "- Backing up xkb-options (keyboard layout tweaks)..."
-gsettings get org.gnome.desktop.input-sources xkb-options > "$BACKUP_DIR/xkb-options.conf"
-
 echo "- Backing up window-manager keybindings..."
 dconf dump /org/gnome/desktop/wm/keybindings/ > "$BACKUP_DIR/wm-keybindings.dconf"
+
+echo "- Backing up desktop-interface keybindings..."
+dconf dump /org/gnome/desktop/interface/ > "$BACKUP_DIR/interface.dconf"
+
+echo "- Backing up xkb-options (keyboard layout tweaks)..."
+gsettings get org.gnome.desktop.input-sources xkb-options > "$BACKUP_DIR/xkb-options.conf"
 
 echo "Backup complete. Files saved to $BACKUP_DIR"
--- scripts/hotkeys-restore.sh
+++ scripts/hotkeys-restore.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
-echo "- Restore ALL media keys (including default + custom)..."
+echo "- Restore media keys"
 dconf load /org/gnome/settings-daemon/plugins/media-keys/ < ~/scripts/gnome-keybindings-backup/media-keys.dconf
-
-echo "- Restore xkb-options (keyboard layout tweaks)..."
-gsettings set org.gnome.desktop.input-sources xkb-options "$(cat ~/scripts/gnome-keybindings-backup/xkb-options.conf)"
 
 echo "- Restoring window-manager keybindings..."
 dconf load /org/gnome/desktop/wm/keybindings/ < ~/scripts/gnome-keybindings-backup/wm-keybindings.dconf
+
+echo "- Restoring interface keybindings..."
+dconf load /org/gnome/desktop/interface/ < ~/scripts/gnome-keybindings-backup/interface.dconf
+
+echo "- Restore xkb-options (keyboard layout tweaks)..."
+gsettings set org.gnome.desktop.input-sources xkb-options "$(cat ~/scripts/gnome-keybindings-backup/xkb-options.conf)"
 
 echo "Restore complete."
