4152cdde41eba94060eac26bc3e7b0f7ca487f66
Philippe Lafrance <phil.laf@gmail.com>
2025-06-20 01:00:32 -0400

    Add fix-permissions alias + docker hard restart alias

--- .bashrc.d/aliases.bash
+++ .bashrc.d/aliases.bash
@@ -1,5 +1,7 @@
 alias space="du -h . | grep '[0-9\.]\+G'"
 
+alias al="vim ~/.bashrc.d/aliases.bash"
+
 alias cos="gh copilot suggest"
 alias coe="gh copilot explain"
 
@@ -11,6 +13,7 @@ alias cr="config difftool HEAD"
 alias dc="docker-compose"
 alias dcd="docker stop \$(docker ps -a -q); docker rm \$(docker ps -a -q); docker volume rm \$(docker volume ls -q); docker network rm \$(docker network ls -q)"
 alias dp='docker ps -a --format "table {{.Names}}\t{{.RunningFor}}\t{{.Status}}\t{{.Ports}}"'
+alias dcr="docker compose down && docker compose up --build"
 
 
 alias g='git'
@@ -22,3 +25,6 @@ alias vim='nvim'
 alias vimdiff='nvim -d'
 
 alias config='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
+
+# fix-permissions {foldername}
+alias fix-permissions='f() { sudo chown -R $USER:$USER "$1" && sudo find "$1" -type d -exec chmod 755 {} \; && sudo find "$1" -type f -exec chmod 644 {} \;; }; f'
